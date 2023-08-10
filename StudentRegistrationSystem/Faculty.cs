89fc7936b9c432c893c25bab4856936d6e709d47
Akshay Ramachandran <akshayzymbo79@gmail.com>
2023-08-10 16:33:36 -0400

    Update Faculty.cs

--- StudentRegistrationSystem/Faculty.cs
+++ StudentRegistrationSystem/Faculty.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 
 namespace StudentRegistrationSystem
 {
+    //faculty class
     public class Faculty
     {
-
+//declaring variables for storing faculty id ,first name,last name email and phone
         private string faultyId;
 
         private string firstName;
@@ -19,7 +20,7 @@ namespace StudentRegistrationSystem
         private string email;
 
         private string phone;
-
+//constructor for the faculty class
         public Faculty(string faultyId, string firstName, string lastName, string email, string phone)
         {
             this.FaultyId = faultyId;
@@ -28,13 +29,13 @@ namespace StudentRegistrationSystem
             this.Email = email;
             this.Phone = phone;
         }
-
+//property for the faculty id ,first name,last name email and phone.
         public string FaultyId { get => faultyId; set => faultyId = value; }
         public string FirstName { get => firstName; set => firstName = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string Email { get => email; set => email = value; }
         public string Phone { get => phone; set => phone = value; }
-
+//overriding the ToString method
         public override string ToString()
         {
             return $"Faculty ID: {FaultyId}\nFirst Name: {FirstName}\nLast Name: {LastName}\nEmail: {Email}\nPhone: {Phone}\n";
